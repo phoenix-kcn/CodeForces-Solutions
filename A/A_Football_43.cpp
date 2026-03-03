b49dd23bacaf18de7e8711f7db6325de26c15efc
Shahadat Hossain <sht.kcn@gmail.com>
2026-03-03 19:28:41 +0600

    43A solved

--- A/A_Football_43.cpp
+++ A/A_Football_43.cpp
@@ -1,37 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+using ll = long long;
+using vi = vector<int>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+
+#define pb push_back
 #define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+
 
 void solve() {
+    
     int n;
     cin >> n;
-
-    vector<string> ss(n);
-    vector<int> freq(256, 0);
-
-    for(int i = 0; i < n; i++)
-        cin >> ss[i];
-
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < ss[i].size(); j++) {
-            freq[(unsigned char)ss[i][j]]++;
-        }
-    }
-
-    int max_freq = *max_element(all(freq));
     
-    for(int k = 65; k < 256; k++) {
-        if(freq[k] == max_freq) {
-            cout << (char)k;
+    // Main logic goes here
+    vs teams(n);
+    string answer;
+    int score = 0; 
+    for(int i=0; i<n; i++) cin >> teams[i];
+
+    for(int i=0; i<n; i++){
+        int counter = 0; 
+        for(int j=0; j<n; j++){
+            if(teams[i] == teams[j]) counter++;
+        }
+        if(counter > score){
+            score = counter;
+            answer = teams[i];
         }
     }
+    cout << answer;
 }
 
+
 int main() {
+    
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
     solve();
+
     return 0;
 }
